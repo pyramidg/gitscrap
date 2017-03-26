4ed8edbf428ddedf38d8bfa8bd84086ab734017a
studioplace <webstudioplace.com>
2017-03-25 20:41:59 -0400

    practice

--- scrap/app/app.js
+++ scrap/app/app.js
@@ -1,14 +1,31 @@
+
+
 var express = require('express');
 var app = express();
 
 
-
 app.get('/', function(req, res) {
-  res.send('1st Step Done!');
+  res.send('Got it again!');
 });
-// If you set it you have to get it
+
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-  console.log("listening on port 3000");
+  console.log('listening on port 3000');
 });
+
+
+// var express = require('express');
+// var app = express();
+//
+//
+//
+// app.get('/', function(req, res) {
+//   res.send('1st Step Done!');
+// });
+// // If you set it you have to get it
+// app.set('port', process.env.PORT || 3000);
+//
+// var server = app.listen(app.get('port'), function() {
+//   console.log("listening on port 3000");
+// });
