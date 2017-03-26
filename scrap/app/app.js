e5495af113e7406a3950059cdd6a602c7de7bf7c
studioplace <webstudioplace.com>
2017-03-25 20:18:05 -0400

    server build

---
+++ scrap/app/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', function(req, res) {
+  res.send('1st Step Done!');
+});
+// If you set it you have to get it
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log("listening on port 3000");
+});
