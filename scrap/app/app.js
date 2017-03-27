6093dcae10006be67c3d77962c31d42b510aa52a
studioplace <webstudioplace.com>
2017-03-26 21:35:34 -0400

    update

--- scrap/app/app.js
+++ scrap/app/app.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
 
 
 // app.get('/', function (req, res) {
@@ -13,14 +14,15 @@ var app = express();
 
 // Add Routes
 app.use(require('./routes/index'));
+app.use(require('./routes/studio'));
 
-app.get('/studio', function (req, res) {
-   res.send(`Studio Route`);
-});
+// makes data files availible to routes
+app.set('appData', dataFile);
+
 
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.set('port'), function () {
-  console.log('log out on 3 stacks');
+  console.log('log out on', app.get('port'));
 });
--- scrap/app/routes/studio.js
+++ scrap/app/routes/studio.js
@@ -2,11 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/studio', function () {
-  res.send('studio route');
+
+router.get('/studio/:webStudioid', function (req, res) {
+
+  var dataFile = req.app.get('appData');
+  var webStudio = dataFile.directory[req.params.webStudioid];
+
+  res.send(`
+      <h1>${webStudio.email}</h1>
+      <h1>${webStudio.first_name}</h1>
+
+    `);
 
 });
-
 
 
 module.exports = router;
